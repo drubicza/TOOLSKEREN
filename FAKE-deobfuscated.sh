485e2a9bdbf745bb95518e67eeea400ee000c787
drubicza <id.slacker@gmail.com>
2019-09-03 08:54:26 +0800

    Add deobfuscated script

---
+++ FAKE-deobfuscated.sh
@@ -0,0 +1,387 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+clear
+echo $ku"Nama : "
+read nama
+echo $ku"Selamat Datang $nama di Tools saya"
+echo $ku"Semoga senang dengan tools saya"
+sleep 5
+clear
+echo $ku"Tools ini berfungsi untuk mempermudah penginstallan"
+echo $me"Note : Jika belum menginstall bahan ketik n"
+echo $me"Jika sudah menginstall bahan bahan ketik y"
+echo 
+echo $ku"Apakah kamu sudah menginstall bahan (y/n)?"
+read conf
+if [ $conf = n ]
+then
+clear
+pip2 install urllib3 chardet certifi idna requests
+pip2 install mechanize
+pip2 install requests
+pkg install curl
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+pkg install ruby -y
+pkg install wget
+npm install -g bash-obfuscate
+pip2 install bs4
+pip2 install termcolor
+pkg install openssh
+pkg install openssl
+pkg install gem
+gem install lolcat
+pip2 install lolcat
+pkg install figlet
+pkg install neofatch
+fi
+
+if [ $conf = y ]
+then
+clear
+fi
+clear
+clear
+echo $cy"╔════════════════════════════════════════╗"
+echo $cy"║"$ku "  AUTHOR  : JHOSUA SAUT MARULI         "$cy"║"
+echo $cy"║"$ku "  CHANNEL : Mr.F4K3 YT                 "$cy"║"
+echo $cy"║"$ku "  UPLOAD  : 6 AGUSTUS 2019             "$cy"║"
+echo $cy"║"$ku "  TYPE    : INSTALLER                  "$cy"║"
+echo $cy"╚════════════════════════════════════════╝"
+echo
+echo $pur"[⌛].DATE  : "
+echo "╔════════════╗"
+date +║" %D   ║ "
+echo "╚════════════╝"
+echo $pur"[⏳].TIME"
+echo $pur"╔═════════════════╗"
+date +"║%T         ║"
+echo "╚═════════════════╝"
+echo
+echo $pu"╔══════════════════════════════════════╗"
+echo $pu"║"$i" https://github.com/LuciferXtools    " $pu"║"
+echo $pu"╚══════════════════════════════════════╝"
+echo
+echo $pu"╔══════════════════════════╗"
+echo $pu"║"$me"[S]. SUBSCRIBE MY CHANNEL "$pu"║"
+echo $pu"╚══════════════════════════╝"
+echo $pu"╔═════════════════════════════╗"
+echo $pu"║"$i"[C]. CHAT ADMIN VIA WHATSAPP "$pu"║"
+echo $pu"╚═════════════════════════════╝"
+echo
+echo $bi"╔════════════════════════════════════════════╗"
+echo $bi"║"$pu"            MENU          "          $bi " ║" $pu"   STATUS" $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[1]. HACK MASSAL FACEBOOK"         $bi"   ║"$i"    ACTIVE" $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[2]. SADAP CAMERA           "                  $bi"║" $i"   ACTIVE  " $bi "║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[3]. LOCATOR                 "$bi"║"  $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[4]. SPAM-SMS               "$bi" ║"   $i"   ACTIVE"  $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[5]. SPAM CHAT FB         "$bi"   ║"  $i  "  ACTIVE"   $bi "  ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[6]. SPAMCALL              "$bi"  ║"   $i "  ACTIVE"   $bi "  ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[7]. AUTO BRUTE FORCE FB     "$bi"║"   $i"   ACTIVE"    $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[8]. DDOS               " $bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[8]. RED_HAWK          "  $bi"     ║"  $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║" 
+echo $bi"║"$cy"[9]. WEEMAN          " $bi"       ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[10].SPAM GMAIL       " $bi"      ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[11].MBF 4x SPEED    " $bi"       ║ "   $i"  ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[12].YAHOO VULN      " $bi"       ║ "  $i"  ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[13].FACEBOOK INFORMATION  "$bi"  ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[14].METASPLOIT          "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[15].FACEBOK TOOLKIT     "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[16].MARSHALL PYTHON     "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[17].OBFUSCATE BASH      "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[18].PROFIL GUARD FB     "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[19].DEFACE              "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[20].SHELLPHISH          "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[21].DARK IG             "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[21].AUTO REPORT FB V1   "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[22].AUTO REPORT FB V2   "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[23].JADWAL SHOLAT       "$bi"    ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[24].CREATOR SCRIPT DEFACE "$bi"  ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[25].WORDLIST GENERATOR   "$bi"   ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"║════════════════════════════════════════════║"
+echo $bi"║"$cy"[26].HACK CCTV            "$bi"   ║"   $i"   ACTIVE"   $bi"   ║"
+echo $bi"╚════════════════════════════════════════════╝"
+echo
+echo $cy"[E].EXIT TOOLS"
+echo
+echo $me"╭─[PILIH NOMORNYA]"
+read -p "╰─root@./F4K3= " pil;
+
+if [ $pil = 1 ]
+then
+clear
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/Cabdulahi/Fbspam
+cd Fbspam
+bash install.sh
+python2 fbspam.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+apt install axel -y
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+rm SpamCall.php
+axel https://raw.githubusercontent.com/my-teach/nuyul-vip/master/SpamCall.php
+php SpamCall.php
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/LuciferXtools/bruteforce
+cd bruteforce
+python2 fbr.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+git clone https://github.com/LuciferXtools/ddos
+cd ddos
+python2 tpddos.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+git clone https://github.com/revan-ar/mail-spammer
+cd mail-spammer
+cd mail-spammer
+php mail.php
+fi
+
+if [ $pil = 11 ]
+then
+clear
+git clone https://github.com/nasirxo/flb2
+cd flb2
+python2 flb2.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+git clone https://github.com/xHak9x/fbi
+cd fbi
+python2 fbi.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install metasploit
+fi
+if [ $pil = 15 ]
+then
+clear
+git clone https://github.com/warifp/FacebookToolkit
+cd FacebookToolkit
+php run.php
+fi
+
+if [ $pil = 15 ]
+then
+clear
+git clone https://github.com/LuciferXtools/Enpython
+cd Enpython
+python2 enc.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+git clone https://github.com/LuciferXtools/Enpython
+cd Enpython
+python2 enc.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+git clone https://github.com/Syhrularv/obfuscate
+cd obfuscate
+python2 bash.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 19 ]
+then
+clear
+git clone https://github.com/R133F/Defacer
+cd Defacer
+python2 defacer.py
+fi
+
+if [ $pil = 20 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 21 ]
+then
+clear
+git clone https://github.com/SIIL3NT/auto-report
+cd auto-report
+python2 auto-report.py
+fi
+
+if [ $pil = 22 ]
+then
+clear
+git clone https://github.com/PangeranAlvins/Repot3
+cd Repot3
+bash install.sh
+python2 Repot3.py
+fi
+
+if [ $pil = 23 ]
+then
+clear
+git clone https://github.com/FR13ND8/Jadwal-Sholat
+cd Jadwal-Sholat
+sh jadwal.sh
+fi
+
+if [ $pil = 24 ]
+then
+clear
+git clone https://github.com/FR13ND8/create-script-deface
+cd create-script-deface
+python2 create.py
+fi
+
+if [ $pil = 25 ]
+then
+clear
+git clone https://github.com/Cabdulahi/wordlist
+cd wordlist
+python2 word.py
+fi
+
+if [ $pil = 26 ]
+then
+clear
+git clone http://github.com/kancotdiq/ipcs
+cd ipcs
+pip2 install requirements
+python2 scan.py
+fi
+
+if [ $pil = S ]
+then
+am start https://www.youtube.com/channel/UCazNREhgns2n_M0P3PLRp6A
+sleep 1
+clear
+fi
+
+if [ $pil = C ]
+then
+am start https://api.whatsapp.com/send?phone=6281260032271
+sleep 1
+clear
+fi
+
+if [ $pil = E ]
+then
+clear
+exit
+fi
